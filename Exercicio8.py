0a1ffa9f3297e0aa042cfb7b330d4651aff7dbed
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-29 21:47:49 -0300

    Create Exercicio8.py
    nesse commit criei o exercicio 8

---
+++ Exercicio8.py
@@ -0,0 +1,5 @@
+salarioporhora = float(input("Digite o valor da hora trabalhada"))
+quantidadehoras = int(input("Digite quantas horas você trabalhou neste mês"))
+valortotal = salarioporhora * quantidadehoras
+
+print(f"O valor da hora trabalhada que é {salarioporhora} multiplicado pela quantidade de horas trabalhadas que é {quantidadehoras} é: R$ {valortotal}")
