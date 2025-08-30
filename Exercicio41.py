39ba4367d84d4ac17d14ae70543c6b38579b5d61
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-29 23:21:53 -0300

    Create Exercicio41.py
    nesse commit fiz o exercicio 41

---
+++ Exercicio41.py
@@ -0,0 +1,28 @@
+numero1 = float(input("Digite um número: "))
+numero2 = float(input("Digite outro número: "))
+op = input("Digite qual operação (+, -, * ou /) deseja realizar: ")
+if (op == '+'):
+    resultado = numero1 + numero2
+elif (op == '-'):
+    resultado = numero1 - numero2
+elif (op == '*'):
+    resultado = numero1 * numero2
+elif (op == '/'):
+    resultado = numero1 / numero2
+ 
+print("\nO resultado é: ")
+ 
+if ((resultado//1) % 2 == 0):
+    print("Par")
+else:
+    print("Ímpar")
+ 
+if (resultado >= 0):
+    print("Positivo")
+else:
+    print("Negativo")
+ 
+if (resultado % 1 == 0):
+    print("Inteiro")
+else:
+    print("Decimal")
