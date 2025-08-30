a2b522cb800b1bc6c82f725e646a254b62b6ed74
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-29 21:51:30 -0300

    Create Exercicio10.py
    nesse commit fiz o exercicio10

---
+++ Exercicio10.py
@@ -0,0 +1,3 @@
+grausCelsius = float(input('Digite a temperatura em Celsius: '))
+grausFarenheit = ((grausCelsius * 9) / 5) + 32
+print(f'{grausCelsius:.2f} graus Celsius correspondem a {grausFarenheit:.2f} graus Farenheit')
