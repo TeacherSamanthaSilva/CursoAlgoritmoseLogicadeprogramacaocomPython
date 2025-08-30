29830b2d85677356f8caa9136148a9dc1068cceb
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-29 23:10:21 -0300

    Create Exericicio31.py
    nesse commit fiz o exercicio 31

---
+++ Exericicio31.py
@@ -0,0 +1,21 @@
+diaInt = int(input('Digite o número do dia da semana: '))
+diaStr = ''
+if diaInt == 1:
+    diaStr = 'Domingo'
+elif diaInt == 2:
+    diaStr = 'Segunda'
+elif diaInt == 3:
+    diaStr = 'Terça'
+elif diaInt == 4:
+    diaStr = 'Quarta'
+elif diaInt == 5:
+    diaStr = 'Quinta'
+elif diaInt == 6:
+    diaStr = 'Sexta'
+elif diaInt == 7:
+    diaStr = 'Sábado'
+ 
+if diaStr == '':
+    print('Valor inválido')
+else:
+    print(f'O dia correspondente é {diaStr}')
