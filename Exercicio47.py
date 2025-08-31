0b1e55ac6ed9d2c399084e4f75c7297cf13ac9a3
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2025-08-31 18:14:28 -0300

    Create Exercicio47.py
    nesse commit fiz o exercicio 47

---
+++ Exercicio47.py
@@ -0,0 +1,6 @@
+usuario = input("Digite o nome de usuario: ")
+senha = input("Digite a senha: ")
+while (usuario == senha):
+    print("Nome de usuario nao pode ser igual a senha!")
+    usuario = input("Digite o nome de usuario: ")
+    senha = input("Digite a senha: ")
